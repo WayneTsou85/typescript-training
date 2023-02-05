798d573a0cb05b885008832d43f24cf5eed14f2e
Wayne Tsou <wayne850109@gmail.com>
2023-02-05 17:33:00 +0800

    add sample type

---
+++ 02-variables/sample-type.ts
@@ -0,0 +1,13 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Anump";
+let lastName: string = 'Kumar';
+
+// break it!
+found = 0;
+firstName = 1;
+lastName = true;
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log(`Hi ${firstName} ${lastName}`)
